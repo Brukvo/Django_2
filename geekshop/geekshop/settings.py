a56cfa85c06e68eabc2b240d2b777a5d40ec34f4
Brukvo <smullerin@mail.ru>
2022-01-04 17:57:56 +0900

    security

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3!qm%#5%$5g0-(%vu4%j@^x9(nvvh5h*wm^kngwb7dyq&qx4*c'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
